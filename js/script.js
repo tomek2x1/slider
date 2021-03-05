2fadea0c049d5283968f80b09f43d54c9ab0880e
Tomasz Lewandowski <tomek2x1@wp.pl>
2021-03-05 14:15:57 +0100

    simply slider

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
+
+
+//   $(document).ready(function() {
+//     $('#autoWidth').lightSlider({
+//         autoWidth:true,
+//         loop:true,
+//         onSliderLoad: function() {
+//             $('#autoWidth').removeClass('cS-hidden');
+//         } 
+//     });  
+//   });
